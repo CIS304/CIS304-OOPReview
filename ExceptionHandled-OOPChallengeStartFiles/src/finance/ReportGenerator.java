201ae8ea6a8445dc6248a576437fc4a2add2d9ed
MirandaO6 <mirandaortiz611@gmail.com>
2016-01-13 19:19:54 -0800

    Added summary in reportgenerator

--- ExceptionHandled-OOPChallengeStartFiles/src/finance/ReportGenerator.java
+++ ExceptionHandled-OOPChallengeStartFiles/src/finance/ReportGenerator.java
@@ -1,5 +1,7 @@
 package finance;
 
+import java.text.NumberFormat;
+
 /**
  * Interface for generating summary reports for financial calculations.
  * @author JAM
@@ -12,5 +14,21 @@ public interface ReportGenerator {
      * @return text summary report of the financial calculation
      */
     public String generateReport ();
- 
+    
+    NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    NumberFormat percentFormat = NumberFormat.getPercentInstance();
+    
+    String summary = "Loan Payment Summary\n";
+           summary += "Purchase Amount: " + currencyFormat.format() + "\n";
+           summary += "Down Payment: " + currencyFormat.format() + "\n";
+           summary += "Amount Financed: " + currencyFormat.format() + "\n";
+           summary += "APR: " + percentFormat.format() + "\n";
+           summary += "Compounding: " +  + "\n";
+           summary += "Loan Duration (years): " +  + "\n";
+           summary += "Payment (monthly): " + currencyFormat.format() + "\n";
+           
+           
+           
 }
+
+
